59e5002cd97c05ed5c9c603456a8e5e07b865231
weiyilimm <lwy_1011@hotmail.com>
2020-02-12 01:10:09 +0000

    Chapter 6 Done, but can't url doesn't match

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
+
+class PageAdmin(admin.ModelAdmin):
+    list_display=('title','category','url')
+    def __str__(self):
+        return self.list_display
+    
+admin.site.register(Page,PageAdmin)
 # Register your models here.
