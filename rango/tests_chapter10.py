d8e736718468dc0363ce9e845184fc403b6a891c
weiyilimm <lwy_1011@hotmail.com>
2020-02-13 23:06:37 +0000

    Chapter 10 done, but db.operational error superuser table not found

---
+++ rango/tests_chapter10.py
@@ -0,0 +1,95 @@
+import os
+import re
+import rango.models
+from rango import forms
+from populate_rango import populate
+from datetime import datetime, timedelta
+from django.db import models
+from django.test import TestCase
+from django.conf import settings
+from django.urls import reverse, resolve
+from django.contrib.auth.models import User
+from django.forms import fields as django_fields
+
+FAILURE_HEADER = f"{os.linesep}{os.linesep}{os.linesep}================{os.linesep}TwD TEST FAILURE =({os.linesep}================{os.linesep}"
+FAILURE_FOOTER = f"{os.linesep}"
+
+f"{FAILURE_HEADER} {FAILURE_FOOTER}"
+
+
+class Chapter10ConfigurationTests(TestCase):
+    """
+    Tests the configuration of the Django project -- can cookies be used, at least on the server-side?
+    """
+    def test_middleware_present(self):
+        """
+        Tests to see if the SessionMiddleware is present in the project configuration.
+        """
+        self.assertTrue('django.contrib.sessions.middleware.SessionMiddleware' in settings.MIDDLEWARE)
+    
+    def test_session_app_present(self):
+        """
+        Tests to see if the sessions app is present.
+        """
+        self.assertTrue('django.contrib.sessions' in settings.INSTALLED_APPS)
+
+
+class Chapter10SessionPersistenceTests(TestCase):
+    """
+    Tests to see if session data is persisted by counting up the number of accesses, and examining last time since access.
+    """
+    def test_visits_counter(self):
+        """
+        Tests the visits counter.
+        Artificially tweaks the last_visit variable to force a counter increment.
+        """
+        for i in range(0, 10):
+            response = self.client.get(reverse('rango:index'))
+            session = self.client.session
+
+            self.assertIsNotNone(session['visits'])
+            self.assertIsNotNone(session['last_visit'])
+
+            # Get the last visit, and subtract one day.
+            # Forces an increment of the counter.
+            last_visit = datetime.now() - timedelta(days=1)
+
+            session['last_visit'] = str(last_visit)
+            session.save()
+
+            self.assertEquals(session['visits'], i+1)
+
+class Chapter10ViewTests(TestCase):
+    """
+    Tests the views manipulated for Chapter 10.
+    Specifically, we look for changes to the index and about views.
+    """
+    def test_index_view(self):
+        """
+        Checks that the index view doesn't contain any presentational logic for showing the number of visits.
+        This should be removed in the final exercise.
+        """
+        response = self.client.get(reverse('rango:index'))
+        content = response.content.decode()
+
+        self.assertTrue('visits:' not in content.lower(), f"{FAILURE_HEADER}The index.html template should not contain any logic for displaying the number of views. Did you complete the exercises?{FAILURE_FOOTER}")
+    
+    def test_about_view(self):
+        """
+        Checks to see if the about view has the correct presentation for showing the number of visits.
+        """
+        response = self.client.get(reverse('rango:index'))  # Call this first to ensure the counter is set.
+        response = self.client.get(reverse('rango:about'))
+        content = response.content.decode()
+
+        self.assertTrue('Visits: 1' in content, f"{FAILURE_HEADER}In your about.html template, please check that you have the correct output for displaying the number of visits. Capital letters matter. Otherwise, check your about() view and the cookie handling logic.{FAILURE_FOOTER}")
+    
+    def test_visits_passed_via_context(self):
+        """
+        Checks that the context dictionary contains the correct values.
+        """
+        response = self.client.get(reverse('rango:index'))  # Set the counter!
+        self.assertNotIn('visits', response.context, f"{FAILURE_HEADER}The 'visits' variable appeared in the context dictionary passed by index(). This should be removed, as per the exercises for Chapter 10.{FAILURE_FOOTER}")
+
+        response = self.client.get(reverse('rango:about'))
+        self.assertIn('visits', response.context, f"{FAILURE_HEADER}We couldn't find the 'visits' variable in the context dictionary for about(). Check your about() implementation.{FAILURE_FOOTER}")
--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,7 @@ from rango.forms import UserForm, UserProfileForm
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 def index(request):
 # Construct a dictionary to pass to the template engine as its context.
@@ -19,13 +19,22 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     context_dict['categories'] = category_list
     context_dict['pages']=page_list
+    
+    visitor_cookie_handler(request)
 # Return a rendered response to send to the client.
 # We make use of the shortcut function to make our lives easier.
 # Note that the first parameter is the template we wish to use.
-    return render(request, 'rango/index.html', context=context_dict)
+###    request.session.set_test_cookie()
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 def about(request):
     context_dict={'boldmessage': 'This tutorial has been put together by weiyilimm'}
+###    if request.session.test_cookie_worked():
+###        print("TEST COOKIE WORKED!")
+###       request.session.delete_test_cookie()
+    visitor_cookie_handler(request)
+    context_dict['visits']=request.session['visits']
     return render(request,'rango/about.html',context=context_dict)
 
 def show_category(request, category_name_slug):
@@ -210,6 +219,52 @@ def user_logout(request):
     logout(request)
 # Take the user back to the homepage.
     return redirect(reverse('rango:index'))
+
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+"""def visitor_cookie_handler(request, response):
+# Get the number of visits to the site.
+# We use the COOKIES.get() function to obtain the visits cookie.
+# If the cookie exists, the value returned is casted to an integer.
+# If the cookie doesn't exist, then the default value of 1 is used.
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+# If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+# Update the last visit cookie now that we have updated the count
+        response.set_cookie('last_visit', str(datetime.now()))
+    else:
+# Set the last visit cookie
+        response.set_cookie('last_visit', last_visit_cookie)
+# Update/set the visits cookie
+    response.set_cookie('visits', visits)"""
+    
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+        # Update/set the visits cookie
+    request.session['visits'] = visits
 
 
 
