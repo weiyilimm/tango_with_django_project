757c37cd78c74f9633dde2c0716c54f7a11f86ef
weiyilimm <lwy_1011@hotmail.com>
2020-02-13 19:44:27 +0000

    Chapter 9 done, but db.OperationalError

--- rango/admin.py
+++ rango/admin.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from rango.models import Category, Page
-
+from rango.models import UserProfile
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
-admin.site.register(Category, CategoryAdmin)
 
 class PageAdmin(admin.ModelAdmin):
     list_display=('title','category','url')
     def __str__(self):
         return self.list_display
-    
+
+admin.site.register(UserProfile)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page,PageAdmin)
 # Register your models here.
